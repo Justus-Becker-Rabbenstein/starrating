89b61970c643531d373da69ff4d2b831ba4e5ecc
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2023-01-23 21:02:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
+
+const dinersArray = [
+  {
+    shop: "Mihai's Magarinepalast",
+    rating: 3.5,
+  },
+  {
+    shop: "Nimba's Nudelsalat Cuisine",
+    rating: 4,
+  },
+  {
+    shop: "Simon's Soßenkanister Pommes",
+    rating: 4.5,
+  },
+  {
+    shop: "Justus Juute Jägerschnitzel",
+    rating: 1,
+  },
+]
+
+// <Icon icon="material-symbols:star" color="gold" />
+// <Icon icon="material-symbols:star-half" color="gold" />
+// <Icon icon="material-symbols:star-outline" color="gold" />
+function handleStars(numRating) {
+  const totalAmountOfStars = 5;
+  let displayArray = []
+
+  for (let index = 0; index < totalAmountOfStars; index++) {
+    console.log(index)
+    if (index < Math.trunc(numRating)) {
+      displayArray.push(<Icon icon="material-symbols:star" color="gold" />) 
+    } else if (numRating % 1) {
+      displayArray.push(<Icon icon="material-symbols:star-half" color="gold" />)
+    } else if (index => numRating) {
+      displayArray.push(<Icon icon="material-symbols:star-outline" color="gold" />)
+    }
+  }
+  return displayArray
+}
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {dinersArray.map((diner)=>{ 
+        return (
+          <div>
+          <p>Name: {diner.shop}</p>
+          <p>Rating numeral: {diner.rating}</p>
+          <p>Rating stars: {handleStars(diner.rating)}</p>
+          <hr></hr>
+          </div>
+      )
+      })}
     </div>
   );
 }
